68664228cb6f792107067d054e50f174c24f9f99
Supi <supimon@gmail.com>
2017-07-22 23:39:43 +0530

    made the menu animation responsive

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,65 @@
-var easing = anime({
-    targets: '.menu-div',
-    translateX: [$('.menu-div').position().left, 0],
-    easing: 'easeInOutQuart',
-    delay: 2000
-});
+(function() {
+    var menuDivEasing, hamburgerTimer, currWidth = getCurrWidth(),
+        menuAreaLeftPos = $('.menu-area').position().left;
+    // menu swipe animation
+    $('.hamburger').on('click', function(){
+        if($('.hamburger').hasClass('open')){
+            clearInterval(hamburgerTimer);
+            menuDivEasing = anime({
+                targets: '.menu-area',
+                translateX: [0, menuAreaLeftPos],
+                easing: 'easeInOutQuart',
+                complete: function(){
+                    $('.hamburger').removeClass('open');
+                }
+            });
+        }
+        else{
+            menuDivEasing = anime({
+                targets: '.menu-area',
+                translateX: [menuAreaLeftPos, 0],
+                easing: 'easeInOutQuart',
+                complete: function(){
+                    $('.hamburger').addClass('open');
+                    hamburgerTimer = setInterval(function () {
+                        var arrowTimeline = anime.timeline();
+                        arrowTimeline
+                            .add({
+                                targets: '.hamburger.open .hamburger-inner',
+                                translateX: '-=20',
+                                duration: 800,
+                                easing: 'easeOutQuad'
+                            })
+                            .add({
+                                targets: '.hamburger.open .hamburger-inner',
+                                translateX: '+=20',
+                                duration: 500
+                            });
+                    }, 5000);
+                }
+            });
+        }
+    });
+    // recalculate initial values
+    $(window).on('resize', function(){
+        currWidth = getCurrWidth();
+    });
+    // utility function to determine device
+    function getCurrWidth(){
+        switch ($('.width-calc').width()){
+            case 1570:
+                return 'xxl';
+            case 1170:
+                return 'lg';
+            case 960:
+                return 'md';
+            case 730:
+                return 'sm';
+            case 550:
+                return 'xs';
+            default:
+                return 'xxs';
+        }
+    }
+
+})();
