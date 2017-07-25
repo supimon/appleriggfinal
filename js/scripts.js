c3d38088914c7964f10445e777701a7a06c75d93
Supi <supimon@gmail.com>
2017-07-26 04:52:47 +0530

    added animation to section sliders. made the menu animation responsive

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,20 @@
 (function() {
     var menuDivEasing, hamburgerTimer, currWidth = getCurrWidth(),
-        menuAreaLeftPos = $('.menu-area').position().left;
+        menuAreaLeftPos = {
+            "pos" : $('.menu-area').position().left
+        },
+        leftPos = {
+            '#aboutApplerigg': $('#aboutApplerigg').offset().left,
+            '#ourCompanies' : $('#ourCompanies').offset().left,
+            '#ourBoard' : $('#ourBoard').offset().left
+        };
     // menu swipe animation
     $('.hamburger').on('click', function(){
         if($('.hamburger').hasClass('open')){
             clearInterval(hamburgerTimer);
             menuDivEasing = anime({
                 targets: '.menu-area',
-                translateX: [0, menuAreaLeftPos],
+                translateX: [0, menuAreaLeftPos.pos],
                 easing: 'easeInOutQuart',
                 complete: function(){
                     $('.hamburger').removeClass('open');
@@ -18,7 +25,7 @@
         else{
             menuDivEasing = anime({
                 targets: '.menu-area',
-                translateX: [menuAreaLeftPos, 0],
+                translateX: [menuAreaLeftPos.pos, 0],
                 easing: 'easeInOutQuart',
                 complete: function(){
                     $('.cool-line').addClass('collapse-line');
@@ -44,7 +51,45 @@
     });
     // recalculate initial values
     $(window).on('resize', function(){
-        currWidth = getCurrWidth();
+        clearTimeout($.data(this, 'resizeTimer'));
+        $.data(this, 'resizeTimer', setTimeout(function() {
+            $('.body-section').animate({
+                scrollLeft: 0,
+                easing: "easein"
+            }, 200, function(){
+                currWidth = getCurrWidth();
+                switch(currWidth){
+                    case "xxl":
+                    case "lg":
+                    case "md":
+                    case "sm":
+                        menuAreaLeftPos = {
+                            "pos" : 50 - $(window).width()
+                        };
+                        break;
+                    default:
+                        menuAreaLeftPos = {
+                            "pos" : 30 - $(window).width()
+                        };
+                        break;
+                }
+                clearInterval(hamburgerTimer);
+                menuDivEasing = anime({
+                    targets: '.menu-area',
+                    translateX: [$('.menu-area').position().left, menuAreaLeftPos.pos],
+                    easing: 'easeInOutQuart',
+                    complete: function(){
+                        $('.hamburger').removeClass('open');
+                        $('.cool-line').removeClass('collapse-line');
+                    }
+                });
+                leftPos = {
+                    '#aboutApplerigg': $('#aboutApplerigg').offset().left,
+                    '#ourCompanies' : $('#ourCompanies').offset().left,
+                    '#ourBoard' : $('#ourBoard').offset().left
+                };
+            });
+        }, 250));
     });
     // utility function to determine device
     function getCurrWidth(){
@@ -63,13 +108,53 @@
                 return 'xxs';
         }
     }
-
+    // company lift animation
+    $('.company-details-item').on('mouseover', function(){
+        $(this).children(".detailed").removeClass('dip-company').addClass('lift-company').end()
+            .children(".cool-line").removeClass("raise-line").addClass("sink-line");
+    }).on('mouseout', function(){
+        $(this).children(".detailed").removeClass('lift-company').addClass('dip-company').end()
+            .children(".cool-line").removeClass("sink-line").addClass("raise-line");
+    });
+    // scroll to the intended section
+    $('.section-slider a, .menu-holder a').click(function (e) {
+        e.preventDefault();
+        var id = $(this).attr("href");
+        if($(this).parents('.menu-holder').length) $('.hamburger').trigger('click');
+        $('.section-slider li').removeClass('is-active');
+        $('.'+id.substring(1)).addClass('is-active');
+        $('.body-section').animate({
+            scrollLeft: leftPos[id],
+            easing: "easein"
+        }, 1000);
+    });
+    // highlight slider dots based on scroll position
+    $('.body-section').scroll(function() {
+        clearTimeout($.data(this, 'scrollTimer'));
+        $.data(this, 'scrollTimer', setTimeout(function() {
+            if (($('#ourCompanies').offset().left < ($(window).width()/2)) &&
+               ($('#ourCompanies').offset().left >= -($(window).width()/2)) &&
+                !$('.ourCompanies').hasClass('is-active')) {
+                    $('.section-slider li').removeClass('is-active');
+                    $('.ourCompanies').addClass('is-active');
+            }
+            else if(($('#ourBoard').offset().left < ($(window).width()/2)) &&
+                !$('.ourBoard').hasClass('is-active')){
+                $('.section-slider li').removeClass('is-active');
+                $('.ourBoard').addClass('is-active');
+            }else if(($('#ourCompanies').offset().left >= ($(window).width()/2)) &&
+                !$('.aboutApplerigg').hasClass('is-active')){
+                $('.section-slider li').removeClass('is-active');
+                $('.aboutApplerigg').addClass('is-active');
+            }
+        }, 250));
+    });
+    // image gallery experiments
     $('.img-gallery .img-reel').css({
         'max-height': 'calc('+$('.img-gallery').height()+'px - '+$($('.img-gallery .item')[0]).css('margin-bottom')+')',
         'width' : $($('.img-gallery .item')[0]).width()+'px',
         'left': $($('.img-gallery .item')[2]).position().left+'px'
     });
-
     $('.img-gallery .item').each(function(i){
         $(this).click(function(){
             $(this).css({
@@ -78,17 +163,4 @@
             })
         });
     });
-    // company lift animation
-    $('.company-details-item').on('mouseover', function(){
-        $(this).children(".detailed").removeClass('dip-company').addClass('lift-company').end()
-            .children(".cool-line").removeClass("raise-line").addClass("sink-line");
-    }).on('mouseout', function(){
-        $(this).children(".detailed").removeClass('lift-company').addClass('dip-company').end()
-            .children(".cool-line").removeClass("sink-line").addClass("raise-line");;
-    });
-
-        /*$(this).css({
-            'transform': 'translateX(-'+$(this).position().left+'px) scale(2.12, 2.12)',
-            'transform-origin': '0px 0px'
-        });*/
 })();
